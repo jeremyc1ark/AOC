170e56fc4fc8c5e17d17b2c84b75a2930f6aa23f
Jeremy Clark <shiblisa0@gmail.com>
2020-12-22 13:16:41 -0800

    Day 18

--- day18.py
+++ day18.py
@@ -1,5 +1,6 @@
 import re
-from operator import add, mul, sub, truediv
+import math
+from operator import add, mul
 
 def make_ops(line):
     line = re.findall(r'\d+|\(|\)|\*|\+', line)
@@ -9,7 +10,6 @@ def make_ops(line):
 data = open('input.txt').read().strip().split('\n')
 data = [make_ops(line) for line in data]
 
-opp_ref = {'*': mul, '-': sub, '/': truediv, '+': add}
 
 def cp_index(i, lat, o=1, c=0):
     # Stands for Closing Paren. Finds index of
@@ -25,23 +25,50 @@ def cp_index(i, lat, o=1, c=0):
             return cp_index(i+1, lat, o, c)
     return i - 1
 
-def null_op(a, b):
-    return b
-
-def eval_ops(expr, acc=0, i=0, opp=null_op):
-    if i == len(expr):
-        return acc
-    val = expr[i]
-    if val == '(':
-        cpi = cp_index(i+1, expr)
-        next_term = eval_ops(expr[i+1:cpi])
-        return eval_ops(expr, opp(acc, next_term), cpi+1, None)
-    elif val == ')':
-        return acc
-    elif isinstance(val, int):
-        return eval_ops(expr, opp(acc, val), i+1, None)
-    else:
-        return eval_ops(expr, acc, i+1, opp_ref[val])
 
 def part_1(ops):
+    op_dict = {'+': add, '*': mul}
+    def null_op(a, b):
+        return b
+    def eval_ops(expr, acc=0, i=0, opp=null_op):
+        if i == len(expr):
+            return acc
+        val = expr[i]
+        if val == '(':
+            cpi = cp_index(i+1, expr)
+            next_term = eval_ops(expr[i+1:cpi])
+            return eval_ops(expr, opp(acc, next_term), cpi+1, None)
+        elif val == ')':
+            return acc
+        elif isinstance(val, int):
+            return eval_ops(expr, opp(acc, val), i+1, None)
+        elif val in '*+':
+            return eval_ops(expr, acc, i+1, op_dict[val])
+        else:
+            print("Error: Unknown symbol")
+            return None
     return sum((eval_ops(expr) for expr in ops))
+
+
+def part_2(ops):
+    def eval_ops(expr, i, total_acc, region_acc):
+        # Been having difficulties with using default arguments in
+        # recursion, so we're just using positionals.
+        if i == len(expr):
+            return math.prod(total_acc) * region_acc
+        val = expr[i]
+        if val == '*':
+            total_acc.append(region_acc)
+            return eval_ops(expr, i+1, total_acc, 0)
+        elif val == '+':
+            return eval_ops(expr, i+1, total_acc, region_acc)
+        elif isinstance(val, int):
+            return eval_ops(expr, i+1, total_acc, region_acc+val)
+        elif val == '(':
+            cpi = cp_index(i+1, expr)
+            term = eval_ops(expr[i+1:cpi], 0, list(), 0)
+            return eval_ops(expr, cpi+1, total_acc, region_acc+term)
+        else:
+            print("Error: Unknown symbol")
+            return None
+    return sum((eval_ops(expr, 0, list(), 0) for expr in ops))
