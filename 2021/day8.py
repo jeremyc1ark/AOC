11835041707c46c06a48d0bdf82d936728710fcb
Jeremy Clark <jeremyclark456@gmail.com>
2022-02-24 20:28:21 -0800

    Finished debugging

--- 2021/day8.py
+++ 2021/day8.py
@@ -19,19 +19,7 @@ def part_1():
                 recognizable_numbers += 1
     return recognizable_numbers
 
-lighting_ref = [
-    {0,1,2,4,5,6},
-    {2,5},
-    {0,2,3,4,6},
-    {0,2,3,5,6},
-    {1,2,3,5},
-    {0,1,3,5,6},
-    {0,1,3,4,5,6},
-    {0,2,5},
-    {0,1,2,3,4,5,6},
-    {0,1,2,3,5}
-]
-
+# Get the strings for the digits with a unique number of segments
 def get_easy_digits(patterns):
     digits_to_patterns = dict()
     lens_to_digits = {2:1, 3:7, 4:4, 7:8}
@@ -41,58 +29,18 @@ def get_easy_digits(patterns):
             digits_to_patterns[digit] = pattern
     return digits_to_patterns
 
-all_char_set = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}
+# Flipping the dictionary
+def ntl_to_ltn(ntl):
+    ltn = dict()
+    for key, value in ntl.items():
+        ltn[value] = key
+    return ltn
 
-# This is a disgusting piece of code :/
-# Need a generic soln rather than brute force
-def solve_segments(patterns):
-    # Segment numbers To Letters
-    ntl = dict()
-    known_digits = get_easy_digits(patterns)
-    # Solving for segment 0
-    ntl[0] = known_digits[7].difference(known_digits[1])
-    # Finding the string set for digits 6 and 0
-    for pattern in patterns:
-        print(pattern)
-        if len(pattern) == 6:
-            print('hello')
-            if len(pattern.difference(all_char_set).intersection(known_digits[1])) == 1:
-                
-                known_digits[6] = pattern
-            else:
-                known_digits[0] = pattern
-    # Solving for segment 2
-    ntl[2] = all_char_set.difference(known_digits[6])
-    # Solving for segment 5
-    ntl[5] = known_digits[7].difference({ntl[0], ntl[2]})
-    # Solving for segment 3
-    ntl[3] = known_digits[8].difference(known_digits[0])
-    # Finding the string set for digits 3, solving for segment 6
-    for pattern in patterns:
-        if len(pattern) == 5:
-            diff = pattern.difference({ntl[x] for x in [0,2,3,5]})
-            if len(diff) == 1:
-                known_digits[3] = pattern
-                ntl[6] = diff
-    # Solving for segments 1 and 4
-    one_and_four = known_digits[8].difference(known_digits[3])
-    ntl[1] = one_and_four.intersection(known_digits[4])
-    ntl[4] = one_and_four.difference(ntl[1])
-
-    # Removing set wrappers
+def remove_set_wrappers(ntl):
     for i in range(7):
         ntl[i] = list(ntl[i])[0]
-    return ntl
+    return ntl_to_ltn(ntl)
 
-
-
-    
-
-if __name__ == '__main__':
-    print(part_1())
-    sample = [set(word) for word in "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab".split(" ")]
-    print(solve_segments(sample))
-    
 """         0
     --------
    |        |
@@ -103,5 +51,106 @@ if __name__ == '__main__':
   4|        |5
    |    6   |
    ---------- """
+
+lighting_ref = [
+    {0,1,2,4,5,6},  # 0
+    {2,5},          # 1
+    {0,2,3,4,6},    # 2
+    {0,2,3,5,6},    # 3
+    {1,2,3,5},      # 4 
+    {0,1,3,5,6},    # 5
+    {0,1,3,4,5,6},  # 6
+    {0,2,5},        # 7
+    {0,1,2,3,4,5,6},# 8
+    {0,1,2,3,5,6}   # 9
+]
+
+# Get the strings of all the digits
+def get_all_digit_strings(patterns):
+    # Get digits that are known so far
+    known_digits = get_easy_digits(patterns)
+
+    # Solving for digit 6
+    for pattern in patterns:
+        if len(pattern) == 6:
+            if known_digits[1].difference(pattern):
+                known_digits[6] = pattern
+    # Solving for digit 0 and 9
+    for pattern in patterns:
+        if len(pattern) == 6:
+            if pattern == known_digits[6]:
+                continue
+            elif known_digits[4].difference(pattern):
+                known_digits[0] = pattern
+            else:
+                known_digits[9] = pattern
+
+    # Solving for digit 3
+    for pattern in patterns:
+        if len(pattern) == 5:
+            if not known_digits[7].difference(pattern):
+                known_digits[3] = pattern
+
+    # Solving for digit 2
+    for pattern in patterns:
+        if len(pattern) == 5:
+            if pattern != known_digits[3]:
+                if len(known_digits[4].difference(pattern)) == 2:
+                    known_digits[2] = pattern
+                else:
+                    known_digits[5] = pattern
+    return known_digits
+    
+def solve_segments(patterns):
+    # segment Numbers To Letters
+    ntl = dict()
+
+    # Getting the strings for all the digits
+    known_digits = get_all_digit_strings(patterns)
+
+    # Solving for segment 0
+    ntl[0] = known_digits[7].difference(known_digits[1])
+
+    # Solving for segment 2
+    ntl[2] = known_digits[1].difference(known_digits[5])
+
+    # Solving for segment 5
+    ntl[5] = known_digits[1].difference(known_digits[2])
+
+    # Solving for segment 1
+    ntl[1] = known_digits[9].difference(known_digits[3])
+
+    # Solving for segment 3
+    ntl[3] = known_digits[2].difference(known_digits[0])
+
+    # Solving for segment 4
+    ntl[4] = known_digits[2].difference(known_digits[3])
+
+    # Solving for segment 6
+    known_segments = ''.join([list(x)[0] for x in ntl.values()])
+    ntl[6] = set('abcdefg').difference(known_segments)
+
+    # Removing set wrappers
+    return remove_set_wrappers(ntl)            
+    
+
+
+def decipher_digit(key, digit_string):
+    lighting = {key[char] for char in digit_string}
+    for i, l in enumerate(lighting_ref):
+        if lighting == l:
+            return i
+
+def decipher_line(line):
+    key = solve_segments(line.patterns)
+    int_list = [decipher_digit(key, string) for string in line.output]
+    str_list = [str(x) for x in int_list]
+    return int(''.join(str_list))
+
+def part_2():
+    return sum([decipher_line(line) for line in data])
+
+if __name__ == '__main__':
+    print(part_2())
     
     
