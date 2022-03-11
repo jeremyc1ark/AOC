004dfec7cb7ffeb1c633503902001b80f1d2e131
Jeremy Clark <jeremyclark456@gmail.com>
2022-03-10 20:16:03 -0800

    Finished.

--- 2021/day10.py
+++ 2021/day10.py
@@ -1,10 +1,9 @@
-import collections
+from collections import deque
+from functools import reduce
 
 data = open('input.txt', 'r').read().strip('\n').split('\n')
 
 open_chars = set("([{<")
-close_chars = set(")]}>")
-
 open_to_close = dict(zip(tuple("([{<"), tuple(")]}>")))
 
 penalty_dict = {
@@ -14,10 +13,9 @@ penalty_dict = {
     '>': 25137
 }
 
-
 def test_line(l):
     i = 0
-    stack = collections.deque()
+    stack = deque()
     while i < len(l):
         char = l[i]
         if char in open_chars:
@@ -32,6 +30,42 @@ def test_line(l):
         i += 1
     return 0
 
+def autocomplete(l):
+    i = 0
+    stack = deque()
+    while i < len(l):
+        char = l[i]
+        if char in open_chars:
+            stack.append(open_to_close[char])
+        else:
+            stack.pop()
+        i += 1
+    stack.reverse()
+    return ''.join(stack)
+
+points_dict = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+def line_to_score(line):
+    completion = autocomplete(line)
+    return reduce(lambda x, y: x * 5 + points_dict[y], completion, 0)
+
+def get_middle(score_list):
+    score_list = sorted(score_list)
+    i = (len(score_list) - 1) / 2
+    return score_list[int(i)]
+
+def part_1():
+    return sum(map(test_line, data))
+
+def part_2():
+    valid_lines = filter(lambda x: test_line(x) == 0, data)
+    return get_middle(map(line_to_score, valid_lines))
         
 if __name__ == '__main__':
-    print(sum(map(test_line, data)))
+    print(part_1())
+    print(part_2())
