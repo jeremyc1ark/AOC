cf71cce3da88c8b780ab3a509db4962aef1ebb3e
jeremy <u1400306@utah.edu>
2023-05-15 08:28:02 -0700

    Day 1 part 2

--- y2022/src/day_1.rs
+++ y2022/src/day_1.rs
@@ -1,18 +1,18 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-#[allow(dead_code)]
-pub fn part_1() -> i32 {
+
+fn get_elf_list() -> Vec<i32> {
     let file: File = File::open("input.txt").expect("Failed to open file.");
     let reader: BufReader<File> = BufReader::new(file);
 
-    let mut highest_elf: i32 = 0;
+    let mut elf_list: Vec<i32> = Vec::new();
     let mut current_elf: i32 = 0;
 
     for line in reader.lines() {
         if let Ok(s) = line {
             if s.trim().is_empty() {
-                highest_elf = if current_elf > highest_elf {current_elf} else {highest_elf};
+                elf_list.push(current_elf);
                 current_elf = 0;
             } else {
                 let meal: i32 = s.parse::<i32>().unwrap();
@@ -20,6 +20,42 @@ pub fn part_1() -> i32 {
             }
         }
     }
-    highest_elf = if current_elf > highest_elf {current_elf} else {highest_elf};
+    elf_list.push(current_elf);
+
+    elf_list
+}
+
+#[allow(dead_code)]
+pub fn part_1() -> i32 {
+    let elf_list: Vec<i32> = get_elf_list();
+    let mut highest_elf: i32 = 0;
+
+    for elf in elf_list {
+        if elf > highest_elf {
+            highest_elf = elf;
+        }
+    }
     highest_elf
+}
+
+/*
+Takes a list of elves and a new elf. Adds the new elf to the list if
+it's greater than any of its elements.
+ */
+fn prioritize_elves(top_elves: &mut Vec<i32>, new_elf: &i32) {
+    top_elves.push(*new_elf);
+    top_elves.sort_by(|a, b| b.cmp(a));
+    top_elves.pop();
+}
+
+#[allow(dead_code)]
+pub fn part_2() -> i32 {
+    let elf_list: Vec<i32> = get_elf_list();
+    let mut top_elves: Vec<i32> = elf_list[0..=2].to_vec();
+
+    for elf in &elf_list[3..] {
+      prioritize_elves(&mut top_elves, elf);
+    }
+
+    top_elves.iter().sum()
 }
--- y2022/src/main.rs
+++ y2022/src/main.rs
@@ -1,6 +1,6 @@
 mod day_1;
 
 fn main() {
-    let result: i32 = day_1::part_1();
+    let result: i32 = day_1::part_2();
     println!("Result: {}", result);
 }
